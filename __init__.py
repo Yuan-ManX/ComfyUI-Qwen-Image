3d1e51d5bc15b0eedfb213031a0175c634200f03
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-08-05 11:09:03 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+from .nodes import LoadQwenImageModel, LoadQwenImagePrompt, LoadQwenImageNegativePrompt, QwenImage, SaveQwenImage
 
+NODE_CLASS_MAPPINGS = {
+    "LoadQwenImageModel": LoadQwenImageModel,
+    "LoadQwenImagePrompt": LoadQwenImagePrompt,
+    "LoadQwenImageNegativePrompt": LoadQwenImageNegativePrompt,
+    "QwenImage": QwenImage,
+    "SaveQwenImage": SaveQwenImage,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadQwenImageModel": "Load Qwen Image Model",
+    "LoadQwenImagePrompt": "Load Qwen Image Prompt",
+    "LoadQwenImageNegativePrompt": "Load Qwen Image Negative Prompt",
+    "QwenImage": "Qwen Image",
+    "SaveQwenImage": "Save Qwen Image",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
